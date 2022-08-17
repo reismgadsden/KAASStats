fd0a1bc15da499dd43d3cb39d11b2638fbbaaea9
Reis Gadsden <rgadsden287@gmail.com>
2022-08-17 17:50:12 -0400

    First Commit

---
+++ KAASStats.py
@@ -0,0 +1,24 @@
+"""
+This program will break down BRITE hierarchies created by KAAS (KEGG Automatic Annotation Server)
+
+author: Reis Gadsden 17-08-2022
+version: 17-08-2022
+last modified: Reis Gadsden 17-08-2022
+github: https://github.com/reismgadsden/KAASStats
+"""
+import sys
+import getopt
+import numpy
+import matplotlib.pyplot as plt
+
+
+def main():
+    pass
+
+
+def read_brite(infile):
+    pass
+
+
+if __name__ == "__main__":
+    main()
